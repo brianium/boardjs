320e4fd6b256024d73ac7de5e0f24241be38268c
Brian Scaturro <scaturrob@gmail.com>
2015-01-05 15:36:54 -0500

    Tile.on and Tile.off element event delegation

--- test/spec/tile.js
+++ test/spec/tile.js
@@ -3,12 +3,36 @@ var Tile = Board.Tile;
 describe('Tile', function() {
   beforeEach(function() {
     this.element = document.createElement('div');
+    this.tile = new Tile(this.element);
   });
 
-  describe('#new', function() {
-    it('should be constructed with an element', function() {
-      var tile = new Tile(this.element);
-      expect(tile.element).not.toBeUndefined();
+  describe('events', function() {
+
+    beforeEach(function() {
+      //deprecated, but phantom does not support constructors yet
+      this.evt = document.createEvent('Event');
+      this.evt.initEvent('click', true, true);
+      this.click = function() {
+        this.style.color = 'red';
+      };
     });
+
+    describe('.on()', function() {
+      it('should register an event with the tile element', function() {
+        this.tile.on('click', this.click);
+        this.tile.element.dispatchEvent(this.evt);
+        expect(this.tile.element.style.color).toBe('red');
+      });
+    });
+
+    describe('.off()', function() {
+      it('should remove a listener for an event on the tile element', function() {
+        this.tile.on('click', this.click);
+        this.tile.off('click', this.click);
+        this.tile.element.dispatchEvent(this.evt);
+        expect(this.tile.element.style.color).toBe('');
+      });
+    });
+
   });
 });
